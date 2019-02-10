21e96b56bcd7d0fc41493579a4ef9aa63a9c7c09
Jeremy Philipson <jeremyphilipson@gmail.com>
2019-02-10 16:59:00 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const title = "My super React Webpack Babel Setup";
+const app = document.getElementById('app');
+
+ReactDOM.render(<div>{title}</div>, app);
+
+module.hot.accept();
