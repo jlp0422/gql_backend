78e0f43e7a5128ffe3ac4c6b24b2965f3fe45259
Jeremy Philipson <jeremyphilipson@gmail.com>
2019-02-24 18:26:25 -0500

    add organization page with search

--- src/client/App/Navigation/index.js
+++ src/client/App/Navigation/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
+
+import Button from '../../Button'
+import Input from '../../Input'
 
 import * as routes from '../../constants/routes'
 import './style.css'
 
-const Naviagation = () => (
+const Navigation = ({
+	location: { pathname },
+	organizationName,
+	onOrganizationSearch
+}) => (
 	<header className="Navigation">
 		<div className="Navigation-link">
 			<Link to={routes.PROFILE}>Profile</Link>
@@ -12,7 +19,47 @@ const Naviagation = () => (
 		<div className="Navigation-link">
 			<Link to={routes.ORGANIZATION}>Organization</Link>
 		</div>
+		{pathname === routes.ORGANIZATION && (
+			<OrganizationSearch
+				organizationName={organizationName}
+				onOrganizationSearch={onOrganizationSearch}
+			/>
+		)}
 	</header>
 )
 
-export default Naviagation
+class OrganizationSearch extends React.Component {
+	state = {
+		value: this.props.organizationName
+	}
+
+	onChange = ev => {
+		this.setState({ value: ev.target.value })
+	}
+
+	onSubmit = ev => {
+		this.props.onOrganizationSearch(this.state.value)
+		ev.preventDefault()
+	}
+
+	render() {
+		const { value } = this.state
+		return (
+			<div className="Navigation-search">
+				<form onSubmit={this.onSubmit}>
+					<Input
+						color={'white'}
+						type={'text'}
+						value={value}
+						onChange={this.onChange}
+					/>{' '}
+					<Button color={'white'} type={'submit'}>
+						Search
+					</Button>
+				</form>
+			</div>
+		)
+	}
+}
+
+export default withRouter(Navigation)
--- src/client/App/index.js
+++ src/client/App/index.js
@@ -9,11 +9,23 @@ import * as routes from '../constants/routes'
 import './style.css'
 
 class App extends React.Component {
+	state = {
+		organizationName: 'the-road-to-learn-react'
+	}
+
+	onOrganizationSearch = value => {
+		this.setState({ organizationName: value })
+	}
+
 	render() {
+		const { organizationName } = this.state
 		return (
 			<Router>
 				<div className="App">
-					<Navigation />
+					<Navigation
+						organizationName={organizationName}
+						onOrganizationSearch={this.onOrganizationSearch}
+					/>
 					<div className="App-main">
 						<Route
 							exact
@@ -21,7 +33,7 @@ class App extends React.Component {
 							component={() => (
 								<div className="App-content_large-header">
 									{' '}
-									<Organization organizationName={'the-road-to-learn-react'} />
+									<Organization organizationName={organizationName} />
 								</div>
 							)}
 						/>
