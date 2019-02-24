8f71a65be8c5aedb065d626decd70a965faf8f51
Jeremy Philipson <jeremyphilipson@gmail.com>
2019-02-24 17:34:07 -0500

    add pagination for repositories

--- src/client/Profile/queries.js
+++ src/client/Profile/queries.js
@@ -3,13 +3,21 @@ import gql from 'graphql-tag'
 import { REPOSITORY_FRAGMENT } from '../Repository'
 
 export const GET_REPOSITORIES_OF_CURRENT_USER = gql`
-	{
+	query($cursor: String) {
 		viewer {
-			repositories(first: 5, orderBy: { direction: DESC, field: STARGAZERS }) {
+			repositories(
+				first: 5
+				orderBy: { direction: DESC, field: STARGAZERS }
+				after: $cursor
+			) {
 				edges {
 					node {
 						...repository
 					}
+				}
+				pageInfo {
+					endCursor
+					hasNextPage
 				}
 			}
 		}
